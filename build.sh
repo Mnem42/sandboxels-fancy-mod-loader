2733b61c2bdb455da276e4b12fa49596bc0a43ac
Mnem42 <IshaanAbhi25@gmail.com>
2025-07-02 19:27:09 +0100

    Remove build.sh (it's redundant now)

--- build.sh
+++
@@ -1,9 +0,0 @@
-# Build to correct file (after switching out global to globalThis with  sed)
-#
-# If you're asking *why* that's needed, some dependencies use global, and this is just to make it
-# also work on web.
-esbuild --bundle src/mod.ts | sed s/global/globalThis/g > sandboxels/mods/fancy_loader.js
-
-# Show output file size
-echo "Size:"
-du -h sandboxels/mods/fancy_loader.js
