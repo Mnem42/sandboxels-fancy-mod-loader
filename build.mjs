b7a6370bdfe85f57438e890bb8f0537698c07ab8
Mnem42 <IshaanAbhi25@gmail.com>
2025-07-03 12:24:24 +0100

    modify build script to show output size

--- build.mjs
+++ build.mjs
@@ -2,20 +2,22 @@ import * as esbuild from 'esbuild'
 import * as fs from 'fs'
 
 await esbuild.build({
-  entryPoints: ['src/mod/mod.ts'],
-  outfile: "sandboxels/mods/fancy_loader.js",
-  platform: "browser",
+    entryPoints: ['src/mod/mod.ts'],
+    outfile: "sandboxels/mods/fancy_loader.js",
+    platform: "browser",
 
-  bundle: true,
-  logLevel: "error",
+    bundle: true,
+    logLevel: "error",
 
-  banner: {
-    js: "// fancy_loader.js"
-  },
-  define: {
-    global: "globalThis",
-    //external_styles: stylesheet
-  },
+    banner: {
+        js: "// fancy_loader.js"
+    },
+    define: {
+        global: "globalThis",
+        //external_styles: stylesheet
+    },
 }).then((result) =>{
+    let stats = fs.statSync("sandboxels/mods/fancy_loader.js")
     console.log("Build finished")
+    console.log("Build output size: ", stats.size, "(B)")
 })
